5f03fd77bb4276a1ebc9388097ae3bc9ef6a6b89
Cody <ken51717@gmail.com>
2023-09-26 17:25:48 +0800

    feat:新增vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import { resolve } from 'path'
 import path from "path"
+
+import AddLogo from './src/plugins/vite-plugin-add-logo'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AddLogo(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src'),
