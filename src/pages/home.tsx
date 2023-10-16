64d450a7ddb30b33e209bd474060b85e46130e32
Cody <ken51717@gmail.com>
2023-10-16 10:39:41 +0800

    fix:footer樣式

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,17 +4,15 @@ import Menu from '../components/menu'
 
 function Home() {
     return (
-
         <div className='flex flex-col h-full md:flex-row'>
-            <div className='text-center  bg-white flex md:w-xs md:bg-inherit md:w-[12vw]'>
+            <div className='text-center  bg-white flex justify-center md:w-xs md:bg-inherit md:w-[12vw]'>
                 <Menu />
             </div>
             <div className='mt-5 p-2.5 flex items-center  flex-col  md:mt-5 md:w-[85vw]'>
                 <Outlet />
             </div>
-            {/* <div className='mt-auto text-center p-2.5'>footer</div> */}
+            <div className='mt-auto text-center p-2.5 md:hidden'>footer</div>
         </div>
-
     )
 }
 
